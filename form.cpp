1ae740986c49d3a80fa2b72977d5fec0826bc021
Alexander Dergachev <dergachev88@gmail.com>
2021-03-16 09:51:23 +0300

    components created

--- form.cpp
+++ form.cpp
@@ -3,6 +3,28 @@
 Form::Form(QWidget *parent)
     : QWidget(parent)
 {
+    labelR = new QLabel("R");
+    labelL = new QLabel("L");
+    labelC = new QLabel("C");
+    labelM = new QLabel("M");
+    labelG0 = new QLabel("G0");
+    labelG2 = new QLabel("G2");
+    labelU0 = new QLabel("U0");
+    labelI0 = new QLabel("I0");
+
+    doubleSpinBoxR = new QDoubleSpinBox;
+    doubleSpinBoxL = new QDoubleSpinBox;
+    doubleSpinBoxC = new QDoubleSpinBox;
+    doubleSpinBoxM = new QDoubleSpinBox;
+    doubleSpinBoxG0 = new QDoubleSpinBox;
+    doubleSpinBoxG2 = new QDoubleSpinBox;
+    doubleSpinBoxU0 = new QDoubleSpinBox;
+    doubleSpinBoxI0 = new QDoubleSpinBox;
+
+    pushButtonStart = new QPushButton("Start");
+    pushButtonStop = new QPushButton("Stop");
+
+    textEditLog = new QTextEdit;
 }
 
 Form::~Form()
--- form.h
+++ form.h
@@ -2,10 +2,19 @@
 #define FORM_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QDoubleSpinBox>
+#include <QPushButton>
+#include <QTextEdit>
 
 class Form : public QWidget
 {
     Q_OBJECT
+
+    QLabel *labelR, *labelL, *labelC, *labelM, *labelG0, *labelG2, *labelU0, *labelI0;
+    QDoubleSpinBox *doubleSpinBoxR, *doubleSpinBoxL, *doubleSpinBoxC, *doubleSpinBoxM, *doubleSpinBoxG0, *doubleSpinBoxG2, *doubleSpinBoxU0, *doubleSpinBoxI0;
+    QPushButton *pushButtonStart, *pushButtonStop;
+    QTextEdit *textEditLog;
 
 public:
     Form(QWidget *parent = nullptr);
