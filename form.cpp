dcca64d72e3c7a453fdb592bd6aff9e51833c9ae
Alexander Dergachev <dergachev88@gmail.com>
2021-03-16 10:07:41 +0300

    layout composed

--- form.cpp
+++ form.cpp
@@ -1,5 +1,9 @@
 #include "form.h"
 
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+
 Form::Form(QWidget *parent)
     : QWidget(parent)
 {
@@ -25,6 +29,35 @@ Form::Form(QWidget *parent)
     pushButtonStop = new QPushButton("Stop");
 
     textEditLog = new QTextEdit;
+
+    QGridLayout *gridLayoutParameters = new QGridLayout;
+    gridLayoutParameters->addWidget(labelR, 0, 0);
+    gridLayoutParameters->addWidget(labelL, 1, 0);
+    gridLayoutParameters->addWidget(labelC, 2, 0);
+    gridLayoutParameters->addWidget(labelM, 3, 0);
+    gridLayoutParameters->addWidget(labelG0, 4, 0);
+    gridLayoutParameters->addWidget(labelG2, 5, 0);
+    gridLayoutParameters->addWidget(labelU0, 6, 0);
+    gridLayoutParameters->addWidget(labelI0, 7, 0);
+    gridLayoutParameters->addWidget(doubleSpinBoxR, 0, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxL, 1, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxC, 2, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxM, 3, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxG0, 4, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxG2, 5, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxU0, 6, 1);
+    gridLayoutParameters->addWidget(doubleSpinBoxI0, 7, 1);
+
+    QHBoxLayout *layoutButtons = new QHBoxLayout;
+    layoutButtons->addWidget(pushButtonStart);
+    layoutButtons->addWidget(pushButtonStop);
+
+    QVBoxLayout *layoutMain = new QVBoxLayout;
+    layoutMain->addLayout(gridLayoutParameters);
+    layoutMain->addLayout(layoutButtons);
+    layoutMain->addWidget(textEditLog);
+
+    setLayout(layoutMain);
 }
 
 Form::~Form()
