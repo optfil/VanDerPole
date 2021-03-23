6e03d893ac90e5c8a8c964f05d3836b64d54ca66
Alexander Dergachev <dergachev88@gmail.com>
2021-03-23 09:59:19 +0300

    verbose output for the key missed

--- dynsystem.cpp
+++ dynsystem.cpp
@@ -1,10 +1,19 @@
 #include "dynsystem.h"
 
-DynSystem::DynSystem(std::map<std::string, double> params):
+#include <iostream>
+
+DynSystem::DynSystem(std::map<std::string, double> params)
+try:
     L{params.at("L")}, R{params.at("R")}, C{params.at("C")},
     M{params.at("M")}, g0{params.at("g0")}, g2{params.at("g2")},
     t_{0.0}, u_{params.at("U0")}, i_{params.at("I0")}
 {}
+catch(const std::out_of_range& err)
+{
+    std::cerr << "DynSystem constructor: key not found\n"
+              << "needed keys: L, R, C, M, g0, g2, U0, I0\n";
+    throw;
+}
 
 void DynSystem::step(double dt)
 {
