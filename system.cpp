152c11629a8e07ff3d12adb3840d72f3d6d5d3da
Alexander Dergachev <dergachev88@gmail.com>
2021-11-29 13:03:36 +0300

    Integration method improved

--- system.cpp
+++ system.cpp
@@ -2,10 +2,7 @@
 
 void System::make_step(double dt)
 {
-	double dx = y_ * dt;
-	double dy = (-1.0/L_/C_ * (R_*C_ - (M_*S0_-M_*S2_*x_*x_))*y_ - 1.0/L_/C_ * x_) * dt;
-	
-	t_ += dt;
-	x_ += dx;
-	y_ += dy;
+    y_ += (-1.0/L_/C_ * (R_*C_ - (M_*S0_-M_*S2_*x_*x_))*y_ - 1.0/L_/C_ * x_) * dt;
+    x_ += y_* dt;
+    t_ += dt;
 }
